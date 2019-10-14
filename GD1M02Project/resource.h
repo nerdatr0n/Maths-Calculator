55bea64345ab813f3ffc1f645a6ec890737319a2
David Haverland <nerdatron.end@gmail.com>
2019-10-14 21:05:00 +1300

    Got the no inverse and random things working -added herobrine

--- GD1M02Project/resource.h
+++ GD1M02Project/resource.h
@@ -1,7 +1,7 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by GD1M02Project.rc
-
+//
 #define IDOK2                           3
 #define IDOK3                           4
 #define IDCANCEL2                       5
@@ -22,6 +22,7 @@
 #define IDD_DialogQuaternion            105
 #define IDR_MENU1                       106
 #define IDC_EDIT1                       1001
+#define IDC_STATIC1                     1001
 #define IDC_EDIT2                       1002
 #define IDC_EDIT3                       1003
 #define IDC_EDIT4                       1004
@@ -180,9 +181,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
